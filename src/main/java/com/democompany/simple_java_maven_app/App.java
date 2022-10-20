4acb25872c0bfecf4c84ea04b0bd70e6d4e0985d
Anirban Das <ad653157@gmail.com>
2022-10-20 14:48:13 +0530

    Added First Commit

---
+++ src/main/java/com/democompany/simple_java_maven_app/App.java
@@ -0,0 +1,20 @@
+package com.democompany.simple_java_maven_app;
+
+/**
+ * Hello world! code for App.java
+ *
+ */
+public class App 
+{
+	 private final String message = "Hello World!";
+
+	    public App() {}
+
+	   public static void main(String[] args) {
+	        System.out.println(new App().getMessage());
+	    }
+
+	    private final String getMessage() {
+	        return message;
+	    }
+}
